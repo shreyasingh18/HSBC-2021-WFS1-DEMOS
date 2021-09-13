be4c804227121a5df4d8361d9d313822511fcad4
Kishor <kishor1124@gmail.com>
2021-09-13 16:28:18 +0530

    mvc demo

---
+++ WebAppExamples/day10-mvc-demo/src/com/hsbc/controller/EmployeeFindAllServlet.java
@@ -0,0 +1,33 @@
+package com.hsbc.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hsbc.factory.ObjectFactory;
+import com.hsbc.model.beans.Employee;
+import com.hsbc.model.business.EmployeeService;
+
+/**
+ * Servlet implementation class EmployeeFindAllServlet
+ */
+@WebServlet("/EmployeeFindAllServlet")
+public class EmployeeFindAllServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmployeeService service = new ObjectFactory().getServiceInstance();
+		List<Employee> employees = service.fetchEmployees();
+		PrintWriter pw = response.getWriter();
+		pw.print("<html><body>");
+		pw.print(employees);
+		pw.print("</body></html>");
+	}
+}
---
+++ WebAppExamples/day10-mvc-demo/src/com/hsbc/controller/EmployeeFindServlet.java
@@ -0,0 +1,38 @@
+package com.hsbc.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.hsbc.factory.ObjectFactory;
+import com.hsbc.model.beans.Employee;
+import com.hsbc.model.business.EmployeeService;
+
+@WebServlet("/EmployeeFindServlet")
+public class EmployeeFindServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("t1"));
+		EmployeeService service = new ObjectFactory().getServiceInstance();
+		Employee employee = service.fetchEmployee(id);
+		
+		PrintWriter pw = response.getWriter();
+		//pw.print("<html><body>");
+		//pw.print(employee);
+		//pw.print("</body></html>");
+		
+		Gson gson = new GsonBuilder().create();
+		String employeeJSON = gson.toJson(employee).toString(); // converts java object to json
+		response.setContentType("application/json"); 
+		pw.print(employeeJSON);
+	}
+
+}

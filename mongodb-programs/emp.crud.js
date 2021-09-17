b9b5d49e06619f1ebbfa710c3b17c6bcd1bb7c2a
Kishor <kishor1124@gmail.com>
2021-09-17 15:58:26 +0530

    mongdb programs

--- mongodb-programs/emp.crud.js
+++ mongodb-programs/emp.crud.js
@@ -1,4 +1,41 @@
+// need to use mongodb library using require
+var MongoClient = require("mongodb").MongoClient;
+
+// connection url
+var url = "mongodb://localhost:27017/";
+
+// Insert record
 /*
- javascript code can be run using node file.js
+MongoClient.connect(url, function(err, con){
+    if(err) {
+        console.log(err);
+        throw err;
+    } else {
+        var dbCon = con.db("mydb")
+        dbCon.collection("emp")
+        .insertOne({"_id":1, "name":"Alex"}, function(err, res){
+            if(err) {
+                console.log(err)
+                throw err;
+            } else {
+                console.log(res);
+                con.close();
+            }
+        });
+    }
+});
 */
-console.log("Hello World");
+// Find all records
+MongoClient.connect(url, function(err, con){
+    if(err) throw err;
+    else {
+        var dbCon = con.db("mydb");
+        dbCon.collection("emp").find().toArray(function(err, res){
+            if(err) throw err;
+            else {
+                console.log(res);
+                con.close();
+            }
+        });
+    }
+});
